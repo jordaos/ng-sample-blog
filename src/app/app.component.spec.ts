71a06709337e56bad9b5538eaf6fba5b59f08fe7
jordaos <jordao.souza05@gmail.com>
2022-03-25 10:30:25 -0300

    Increase app test coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { FooterComponent } from '@/components/footer/footer.component';
+import { HeaderComponent } from '@/components/header/header.component';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'sample-blog'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample-blog');
+    expect(component.title).toEqual('sample-blog');
+  });
+
+  it('should set page height after view init', () => {
+    // given
+    const headerHeight = component.appHeader._getHeight();
+    const footerHeight = component.appFooter._getHeight();
+
+    // when
+    component.ngAfterViewInit();
+
+    // then
+    expect(component.appContainer.nativeElement.style.minHeight).toBe(`calc(100vh - ${headerHeight + footerHeight}px)`);
   });
 });
